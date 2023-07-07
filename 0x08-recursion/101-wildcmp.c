26fbda1187cfaef6897ec6ed5febf94a0c54b0b3
Humphrey <kinotihum@gmail.com>
2023-07-07 13:25:45 +0000

    Unfinished

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: pointer to the address of the first string
+ * @s2: pointer to the address of the second string
+ *
+ * Return: 1 if strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != '\0')
+	{
+		if (
+	}
+}
